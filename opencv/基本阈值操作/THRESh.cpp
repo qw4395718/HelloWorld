0803ce872fb8a3c6865a26a89d85b3ae5e2f1bb3
zyf <1242703910@qq.com>
2021-05-05 15:10:43 +0800

    //ZYF:提交阈值操作

---
+++ opencv/基本阈值操作/THRESh.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv.hpp>
+using namespace cv;
+using namespace std;
+
+#define UnitTest1
+	Mat src;
+#ifdef UnitTest1
+	int threshold_value = 127;
+	int threshold_maxValue = 255;
+	Mat src_gray, dst;
+	void threshold_Demo(int, void*);
+	const char* output_title = "Output img";
+#endif // UnitTest1
+
+
+int main()
+{
+
+	src = imread("pic.jpg");
+	if (!src.data)
+	{
+		cout << "could not load img..." << endl;
+		return -1;
+	}
+	imshow("Input",src);
+
+#ifdef UnitTest1
+
+
+	namedWindow(output_title, CV_WINDOW_AUTOSIZE);
+
+	createTrackbar("Threshold Value", output_title, &threshold_value, &threshold_maxValue, threshold_Demo);
+	threshold_Demo(0, 0);
+
+#endif // 
+
+
+	waitKey(0);
+	return 0;
+}
+
+void threshold_Demo(int, void*)
+{
+	cvtColor(src, src_gray, CV_RGB2GRAY);
+	threshold(src_gray, dst, threshold_value, threshold_maxValue, THRESH_BINARY);//二值化处理
+	imshow("Output img", dst);
+
+}
